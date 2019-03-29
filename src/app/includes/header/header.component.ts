b21c2d0650dec1763cbcbebfc64f05f448eb62c9
anastahir411 <37661021+anastahir411@users.noreply.github.com>
2019-03-29 19:22:47 +0500

    aasas

--- src/app/includes/header/header.component.ts
+++ src/app/includes/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const $: any;
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -10,6 +11,25 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const mainSearch = $('.main-search');
+    const formSearch = $('.form-search');
+
+    $('#searchIcon').click(function () {
+      $(mainSearch).addClass('active');
+      $('body').addClass('noScroll');
+      $(formSearch).addClass('flipInX');
+
+      setTimeout(function () {
+        $('#input-searchbar').focus();
+      }, 370);
+
+    });
+
+    $('#closeSearch').click(function () {
+      $(mainSearch).removeClass('active');
+      $('body').removeClass('noScroll');
+      $(formSearch).removeClass('flipInX');
+    });
   }
 
 }
--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const $: any;
 @Component({
   selector: 'app-mainpage',
   templateUrl: './mainpage.component.html',
@@ -10,6 +11,27 @@ export class MainpageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const mainSearch = $('.main-search');
+    const formSearch = $('.form-search');
+
+    $('.search-bg').click(function () {
+      $(mainSearch).addClass('active');
+      $('body').addClass('noScroll');
+      $(formSearch).addClass('flipInX');
+
+
+      setTimeout(function () {
+        $('#input-searchbar').focus();
+      }, 370);
+
+
+    });
+
+    $('#closeSearch').click(function () {
+      $(mainSearch).removeClass('active');
+      $('body').removeClass('noScroll');
+      $(formSearch).removeClass('flipInX');
+    });
   }
 
 }
